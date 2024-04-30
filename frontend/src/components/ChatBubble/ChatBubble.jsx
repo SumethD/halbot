b0a29b48c88421e713bbd6c39a8d606da20a8452
Sao <sheku.kanneh12@gmail.com>
2024-04-30 17:27:59 +1000

    update chat interface

---
+++ frontend/src/components/ChatBubble/ChatBubble.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import botIcon from "../../images/boticon.png";
+
+const ChatBubble = ({chatMessage}) => {
+    return (
+        <div>
+            {
+                chatMessage.sender === 'bot' && (
+                    <div className="bot-main-div">
+                        <div><img className="botIcon" src={botIcon}></img></div>
+                        <div className="bot message botmessages-div">{chatMessage.message}</div>
+                    </div>
+                )
+            }
+            {
+                chatMessage.sender === 'user' && (
+                    <div className="user-message-container">
+                        <div className="user message">{chatMessage.message}</div>
+                    </div>
+                )
+            }
+        </div>
+    );
+};
+
+export default ChatBubble;
--- frontend/src/components/ChatInterface.jsx
+++ frontend/src/components/ChatInterface.jsx
@@ -1,101 +1,106 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './ChatInterface.css'; // Import CSS file for styling
 import '../values/colours.css';
 import PromptsPopUp from './PromptsPopUp';
-import botIcon from '../images/boticon.png'
+import botIcon from '../images/boticon.png';
+import {sendMessage} from "../utils/client";
+import ChatBubble from "./ChatBubble/ChatBubble";
 
 const ChatInterface = () => {
-  const [inputMessage, setInputMessage] = useState('');
-  const [chatHistory, setChatHistory] = useState([]);
-  
-  const chatbox = useRef(null);
-  useEffect(() => chatbox.current.scrollIntoView(false), [chatHistory]);
+    const [inputMessage, setInputMessage] = useState('');
+    const [chatHistory, setChatHistory] = useState([]);
 
-  //const example_response = ["Hello I am Hal!", "idk"];
-  const example_response = "Hello I";
+    const chatbox = useRef(null);
+    useEffect(() => chatbox.current.scrollIntoView(false), [chatHistory]);
 
-
-  // method for send through button or press enter
-  const handleSendMessage = async () => {
-    if (inputMessage.trim() !== '') {
-      setChatHistory([...chatHistory, { user: inputMessage, bot: example_response, botType: typeof example_response }]);
-      setInputMessage('');
-    }
-  };
-
-  // method for pop up send
-  const handlePopUpClick = async (message) => {
-    if (message.trim() !== '') {
-      setChatHistory([...chatHistory, { user: message, bot: example_response, botType: typeof example_response }]);
-      setInputMessage('');
-    }
-  };
+    //const example_response = ["Hello I am Hal!", "idk"];
+    const example_response = "Hello I";
 
 
-  const handleInputChange = (e) => {
-    setInputMessage(e.target.value);
-  };
+    // method for send through button or press enter
+    const handleSendMessage = async () => {
+        if (inputMessage.trim() !== '') {
+            setChatHistory([...chatHistory,
+                {
+                    sender: 'user',
+                    message: inputMessage
+                }
+            ]);
 
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      handleSendMessage();
-    }
-  };
+            const res = await sendMessage(inputMessage);
 
-  return (
-    <div className='chat-main-div'>
-      <PromptsPopUp
-        handlePopUpClick={handlePopUpClick}
-      ></PromptsPopUp>
-      <div className="chat-interface">
-        <div className="header-box">HAL BOT</div>
-        <div className="chat-history">
-          <div ref={chatbox}>
-            {chatHistory.map((message, index) => (
-              <div key={index}>
-                {message.botType === 'object' ? (
-                  <div>
-                    <div className='user-message-container'><div className="user message">{message.user}</div></div>
-                    <div className='bot-main-div'>
-                      <div><img className='botIcon' src={botIcon}></img></div>
-                      <div className='botmessages-div'>
-                      {message.bot.map((item, itemIndex) => (
-                        <div className='bot-message-container'><div key={itemIndex} className='bot message'>{item}</div></div>
-                      ))}
-                      </div>
+            console.log(res);
+
+            if (!res || !res.messages) return;
+
+            const botMessages = res.messages;
+
+            const mappedMessages = botMessages.map(message => (
+                {
+                    sender: 'bot',
+                    message: message.content,
+                    ...message
+                }
+            ));
+
+            console.log(mappedMessages)
+
+            setChatHistory([...chatHistory, ...mappedMessages]);
+            setInputMessage('');
+        }
+    };
+
+    // method for pop up send
+    const handlePopUpClick = async (message) => {
+        if (message.trim() !== '') {
+            setChatHistory([...chatHistory, {user: message, bot: example_response, botType: typeof example_response}]);
+            setInputMessage('');
+        }
+    };
+
+
+    const handleInputChange = (e) => {
+        setInputMessage(e.target.value);
+    };
+
+    const handleKeyPress = async (event) => {
+        if (event.key === 'Enter') {
+            await handleSendMessage();
+        }
+    };
+
+    return (
+        <div className="chat-main-div">
+            <PromptsPopUp
+                handlePopUpClick={handlePopUpClick}
+            ></PromptsPopUp>
+            <div className="chat-interface">
+                <div className="header-box">HAL BOT</div>
+                <div className="chat-history">
+                    <div ref={chatbox}>
+                        {chatHistory.map((message, index) => (
+                           <ChatBubble key={index} chatMessage={message} />
+                        ))}
                     </div>
-                  </div>
-                ) : (
-                  <div>
-                    <div className='user-message-container'><div className="user message">{message.user}</div></div>
-                    <div className='bot-main-div'>
-                        <div><img className='botIcon' src={botIcon}></img></div>
-                        <div className="bot message botmessages-div">{message.bot}</div>
+                </div>
+                <div className="message-div">
+                    <div className="message-input">
+                        <input
+                            type="text"
+                            value={inputMessage}
+                            onChange={handleInputChange}
+                            placeholder="Type your message..."
+                            className="input-field"
+                            onKeyDown={handleKeyPress}
+                        />
+                        <button onClick={handleSendMessage} className="send-button">Send</button>
                     </div>
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
+
+                </div>
+            </div>
         </div>
-        <div className="message-div">
-          <div className='message-input'>
-            <input
-              type="text"
-              value={inputMessage}
-              onChange={handleInputChange}
-              placeholder="Type your message..."
-              className="input-field"
-              onKeyDown={handleKeyPress}
-            />
-            <button onClick={handleSendMessage} className="send-button">Send</button>
-          </div>
-          
-        </div>
-      </div>
-    </div>
-    
-  );
+
+    );
 };
 
 export default ChatInterface;
